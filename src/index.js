689e97aae4cafa3d9ba0ef7f57f60bf90a5c0279
Kenneth Tay <kenneth_21@kenneths-mbp.lan>
2024-08-31 12:19:40 +0800

    Basic website layout done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css';
 
 console.log("Hello world");
+const addProject = document.querySelector("#addProject");
+const dialogProject = document.querySelector("#addProjectDialog");
+
+addProject.addEventListener("click", () =>{
+    dialogProject.showModal();
+});
