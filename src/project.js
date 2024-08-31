6ec9207009e622cc1939b6cd1c4edb338ff6fc8f
Kenneth Tay <kenneth_21@kenneths-mbp.lan>
2024-08-31 13:39:01 +0800

    Created a todo class and project class

---
+++ src/project.js
@@ -0,0 +1,53 @@
+import Todo from './todo.js'; // Import the Todo class
+
+class Project {
+    constructor(title, description, dueDate, priority) {
+        this._title = title;
+        this._description = description;
+        this._dueDate = dueDate;
+        this._priority = priority;
+        this._tasks = []; // Array to store Todo tasks
+    }
+
+    // Getters for project properties
+    get getTitle() {
+        return this._title;
+    }
+
+    get getDescription() {
+        return this._description;
+    }
+
+    get getDueDate() {
+        return this._dueDate;
+    }
+
+    get getPriority() {
+        return this._priority;
+    }
+
+    get getTasks() {
+        return this._tasks;
+    }
+
+    // Method to add a new task to the project
+    addTask(task) {
+        if (task instanceof Todo) {
+            this._tasks.push(task);
+        } else {
+            console.error("Invalid task. Must be an instance of Todo.");
+        }
+    }
+
+    // Method to display all project details including tasks
+    displayDetails() {
+        console.log(`Title: ${this.title}`);
+        console.log(`Description: ${this.description}`);
+        console.log(`Due Date: ${this.dueDate}`);
+        console.log(`Priority: ${this.priority}`);
+        console.log(`Tasks:`);
+        this._tasks.forEach(task => task.displayTask());
+    }
+}
+
+export default Project;
---
+++ src/todo.js
@@ -0,0 +1,23 @@
+class Todo {
+    constructor(title, description){
+        this._title = title;
+        this._description = description;
+        this._isCompleted = false;
+    }
+
+    get getTitle(){
+        return this._title;
+    }
+
+    get getDescription(){
+        return this._description;
+    }
+    
+    get getIsCompleted(){
+        return this._isCompleted;
+    }
+
+    changeIsCompleted(){
+        this._isCompleted = !this._isCompleted;
+    }
+}
