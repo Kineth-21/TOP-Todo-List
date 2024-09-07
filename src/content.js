56ee2557321f18d231f41b85431247a6b127254a
Kenneth Tay <kenneth21tay@gmail.com>
2024-09-07 15:28:28 +0800

    Added todo dialog when adding task into project

--- src/content.js
+++ src/content.js
@@ -1,4 +1,5 @@
 const content = document.querySelector("#content");
+const showTodoDialog = document.querySelector("#addTodoDialog");
 
 export function updateContent(project){
     content.textContent = "";   
@@ -6,10 +7,15 @@ export function updateContent(project){
     const contentTitle = document.createElement("div");
     contentTitle.textContent = "Project: " + project._title;
 
+    const addTodo = document.createElement("button");
+    addTodo.textContent ="Add todo"
+    addTodo.addEventListener("click", (event) => {showTodoDialog.showModal()});
+
     const contentDescription = document.createElement("div");
     contentDescription.textContent = "Description: " + project._description;
 
     content.appendChild(contentTitle);
+    content.appendChild(addTodo);
     content.appendChild(contentDescription);
 }
 
